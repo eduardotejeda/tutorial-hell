0d7120420b581a139d22c06a67f1234779bd526b
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-01-28 09:51:57 -0400

    add more examples js

--- web-developer-bootcamp/JS/MoreFunctions/app.js
+++ web-developer-bootcamp/JS/MoreFunctions/app.js
@@ -11,7 +11,7 @@ function bankRobbery() {
 }
 
 // Higher order functions
-
+//Accept other functions as arguments
 function callTwice(func) {
     func();
     func();
@@ -20,4 +20,36 @@ function callTwice(func) {
 function rollDice() {
     const roll = Math.floor(Math.random()*6)+1
     console.log(roll);
+}
+
+//Return a fucntion
+
+function makeBetweenFunc(min, max) {
+    return function (num) {
+        return num >= min && num <= max;
+    }
+}
+
+let isChild = makeBetweenFunc(0,18);
+let isAduld = makeBetweenFunc(19,65);
+let isSenior = makeBetweenFunc(66,120);
+
+//Methods
+
+const myMath = {
+    sum: function (num1, num2) {
+        return num1 + num2;
+    },
+    square (num) {
+        return num * num;
+    }
+}
+
+const hen = {
+    name: "Helen",
+    eggCount:0,
+    layAnEgg() {
+        this.eggCount++
+        return "EGG"
+    }
 }
