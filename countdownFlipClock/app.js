2101c290aecec85240cd9110565e06909faa20d6
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-05-22 05:14:31 -0400

    Complete countdownfliptimer

--- countdownFlipClock/app.js
+++ countdownFlipClock/app.js
@@ -1,4 +1,4 @@
-const targetDate = '2023-09-27T19:00:00';
+const targetDate = '2023-05-22T05:20:00';
 
 function getTimeSegmentElements(segmentElement){
     const segmentDisplay = segmentElement.querySelector(
@@ -37,7 +37,93 @@ function updateSegmentValues(displayElement, overlayElement, value) {
 function updateTimeSegment(segmentElement, timeValue) {
      const segmentElements = getTimeSegmentElements(segmentElement)
 
+    if (parseInt(segmentElements.segmentDisplayTop.textContent, 10) === timeValue) {
+        return;
+    } 
+
      segmentElements.segmentOverlay.classList.add('flip');
+
+     updateSegmentValues(
+        segmentElements.segmentDisplayTop,
+        segmentElements.segmentOverlayBottom,
+        timeValue
+     );
+
+     function finishAnimation() {
+        segmentElements.segmentOverlay.classList.remove('flip');
+        updateSegmentValues(
+            segmentElements.segmentDisplayBottom,
+            segmentElements.segmentOverlayTop,
+            timeValue
+        );
+
+        this.removeEventListener('animateonend', finishAnimation);        
+     }
+
+     segmentElements.segmentOverlay.addEventListener('animationend', finishAnimation);
+};
+
+function updateTimeSection(sectionID, timeValue) { //14
+    const firstNumber = Math.floor(timeValue /10); // 1
+    const secondNumber = timeValue % 10; // 4
+
+    const sectionElement = document.getElementById(sectionID);
+    const timeSegments = sectionElement.querySelectorAll('.time-segment');
+
+    updateTimeSegment(timeSegments[0], firstNumber);
+    updateTimeSegment(timeSegments[1], secondNumber);
+
+    
+}
+
+function getTimeRemaining(targetDateTime) {
+    const nowTime = Date.now(); //
+    const secondsRemaining = Math.floor((targetDateTime - nowTime) / 1000);
+
+    const complete = nowTime >= targetDateTime;
+
+    if (complete) {
+        return {
+            complete,
+            seconds: 0,
+            minutes: 0,
+            hours: 0,
+        };
+    };
+
+
+
+    const hours = Math.floor(secondsRemaining / 60 / 60);
+    const minutes = Math.floor(secondsRemaining / 60 ) - hours * 60;
+    const seconds = secondsRemaining % 60;
+
+    return {
+        complete,
+        seconds,
+        minutes,
+        hours
+    };
 
 
 };
+ 
+function updateAllSegments() {
+    const targetTimeStamp = new Date(targetDate).getTime();
+    const timeRemainingBits = getTimeRemaining(targetTimeStamp)
+
+    updateTimeSection('seconds', timeRemainingBits.seconds);
+    updateTimeSection('minutes', timeRemainingBits.minutes);
+    updateTimeSection('hours', timeRemainingBits.hours);
+
+    return timeRemainingBits.complete;
+}
+
+const countdownTimer = setInterval(()=>{
+    const isComplete = updateAllSegments();
+
+    if(isComplete) {
+        clearInterval(countdownTimer);
+    }
+}, 1000);
+
+updateAllSegments();
