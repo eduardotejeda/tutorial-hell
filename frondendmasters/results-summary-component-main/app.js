f499073458d2972fd3c35e13e84ee944435c852c
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-06-01 06:02:13 -0400

    Add more structure to html

--- frondendmasters/results-summary-component-main/app.js
+++ frondendmasters/results-summary-component-main/app.js
@@ -4,16 +4,13 @@ fetch("data.json")
 })
 .then(function(scores){
     console.log(scores)
+    
     let placeholder = document.querySelector("#data-output");
     let out = "";
 
     for(let score of scores){
         out += `
-        <ul>
-        <li><img src='${score.icon}'></li>
-        <li>${score.category}</li>
-        <li>${score.score}</li>
-        </ul>
+        <li><img src='${score.icon}'> ${score.category} ${score.score} / 100</li>
         `;
        
 
